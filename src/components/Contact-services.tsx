946450677b7523ea9993e7b9aa58c143f500aef0
Avtarsran <official.avtarsran@gmail.com>
2024-09-21 05:49:57 +0530

    made changes in contact-form

--- src/components/Contact-services.tsx
+++ src/components/Contact-services.tsx
@@ -19,30 +19,30 @@ const ContactForm: React.FC = () => {
     return (
         <div
             ref={formRef}
-            className="bg-[#d3d6db] contactForm rounded-lg px-2 py-3 m-2 opacity-0 transform translate-y-5 transition-all duration-1000 ease-in"
+            className="bg-[#d3d6db] contactForm rounded-lg px-2 py-3 m-2 opacity-0 transform translate-y-5 transition-all duration-1000 ease-in sm:shadow"
             id="implementationContact"
         >
             <h2 className="text-2xl text-center font-bold mb-2">Contact Us</h2>
-            <form className="w-full px-5" onSubmit={handleSubmit}>
-                <div className="flex justify-between">
+            <form className="w-full px-2 sm:px-5" onSubmit={handleSubmit}>
+                <div className="flex flex-col sm:flex-row justify-between">
                     <div className="form-group mb-1.5">
                         <label htmlFor="name" className="block text-lg mb-1">Name:</label>
-                        <input type="text" id="name" name="name" className="p-1 mx-1 border border-gray-400 rounded" />
+                        <input type="text" id="name" name="name" className="p-1 mx-0 sm:mx-1 border border-gray-400 rounded w-full sm:w-auto" />
                     </div>
                     <div className="form-group mb-1.5">
                         <label htmlFor="email" className="block text-lg mb-1">Email:</label>
-                        <input type="email" id="email" name="email" className="p-1 border border-gray-400 rounded" />
+                        <input type="email" id="email" name="email" className="p-1 border border-gray-400 rounded w-full sm:w-auto" />
                     </div>
                 </div>
 
-                <div className="flex justify-evenly">
+                <div className="flex flex-col sm:flex-row justify-evenly">
                     <div className="form-group mb-1.5">
                         <label htmlFor="company" className="block text-lg mb-1">Company:</label>
-                        <input type="text" id="company" name="company" className="p-1 mx-1 border border-gray-400 rounded" />
+                        <input type="text" id="company" name="company" className="p-1 mx-0 sm:mx-1 border border-gray-400 rounded w-full sm:w-auto" />
                     </div>
                     <div className="form-group mb-1.5">
                         <label htmlFor="website" className="block text-lg mb-1">Website:</label>
-                        <input type="url" id="website" name="website" className="p-1 border border-gray-400 rounded" />
+                        <input type="url" id="website" name="website" className="p-1 border border-gray-400 rounded w-full sm:w-auto" />
                     </div>
                 </div>
 
